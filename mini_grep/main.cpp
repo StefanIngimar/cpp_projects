942074c344112c5cd15f07de5fdc43a3315bf682
stefanla <stefanla@student.chalmers.se>
2026-09-17 13:57:32 +0200

    basic minigrep

--- mini_grep/main.cpp
+++ mini_grep/main.cpp
@@ -4,6 +4,11 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
+
+    if(argc != 3){
+        cerr << "you have to insert arguments" << endl;
+        return 1;
+    }
     cout << "you have entered " << argc << " arguments:" << endl;
 
     int i = 0;
@@ -11,6 +16,23 @@ int main(int argc, char* argv[]){
         cout << "argument " << i+1 << ": " <<argv[i] << endl;
         i++;
     }
+    string searchTerm = argv[1];
     std::ifstream file(argv[2]);
+
+    cout << "searching for: " << searchTerm << endl;
+
+    string line;
+    size_t lineNumber = 0;
+
+    while(getline(file, line)){
+        lineNumber++;
+        size_t position = line.find(searchTerm);
+
+        if(position != string::npos){
+            cout << "Line " << lineNumber
+                << ", position " << position
+            << ": " << searchTerm << endl;
+        }
+    }
     return 0;
 }
